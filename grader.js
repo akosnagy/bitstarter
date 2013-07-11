a3978daea25ea59d1c9dbd353aa987bd81531203
Akos Nagy-Zambo <akos.nagy@gmail.com>
2013-07-11 00:23:10 +0000

    Added ur support

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "-1";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +38,20 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlData = function(data) {
+    var options = {
+      lowerCaseTags: true
+    };
+    return cheerio.load(data, options);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(data, checksfile) {
+    $ = cheerioHtmlData(data);
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +71,29 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url  <html_file>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.error("%s does not exist. Exiting.", program.url);
+          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        } else {
+          console.error("Processing data from %s", program.url);
+          var data = result.toString();
+          var checkJson = checkHtmlFile(data, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
+      });
+    } else {
+      console.error("Processing data from %s", program.file);
+      var data = fs.readFileSync(program.file);
+      var checkJson = checkHtmlFile(data, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
